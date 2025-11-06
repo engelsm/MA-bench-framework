f109efb1e8359e0e00a41e89937d5d5c761e6a78
engelsm <marlon.engels21@gmail.com>
2025-11-06 17:28:09 +0100

    minimal spmvm workload

---
+++ workloads/sparse_minmal.c
@@ -0,0 +1,61 @@
+// sparse_minimal.c
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int row, col;
+    double val;
+} Entry;
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s matrix.mtx\n", argv[0]);
+        return 1;
+    }
+
+    FILE *f = fopen(argv[1], "r");
+    if (!f) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    // Skip comments
+    char line[256];
+    do {
+        if (!fgets(line, sizeof(line), f)) {
+            fprintf(stderr, "Invalid file\n");
+            return 1;
+        }
+    } while (line[0] == '%');
+
+    int nrows, ncols, nnz;
+    if (sscanf(line, "%d %d %d", &nrows, &ncols, &nnz) != 3) {
+        fprintf(stderr, "Invalid header\n");
+        return 1;
+    }
+
+    Entry *A = malloc(sizeof(Entry) * nnz);
+    for (int i = 0; i < nnz; i++) {
+        fscanf(f, "%d %d %lf", &A[i].row, &A[i].col, &A[i].val);
+        A[i].row--;  // convert to 0-based indexing
+        A[i].col--;
+    }
+    fclose(f);
+
+    double *y = calloc(nrows, sizeof(double));
+
+    // Multiply by vector of 1's: y = A * 1
+    for (int i = 0; i < nnz; i++) {
+        y[A[i].row] += A[i].val;
+    }
+
+    // Print checksum (sum of results)
+    double checksum = 0.0;
+    for (int i = 0; i < nrows; i++) checksum += y[i];
+
+    printf("Checksum: %.6f\n", checksum);
+
+    free(A);
+    free(y);
+    return 0;
+}
