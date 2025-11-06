e65627290e39479c3f4570a7e6359fe51197e894
engelsm <marlon.engels21@gmail.com>
2025-11-06 10:10:55 +0100

    minimal working execution script

--- controller.py
+++ controller.py
@@ -11,8 +11,25 @@ Usage:
     ...
 """
 
-def run_benchmark(cmd, args=[]):
+import argparse
+import subprocess
+
+def run_benchmark(exec_path):
     """Runs benchmark subprocess with given parameters and collects results."""
+    print(f"Executing: {exec_path}")
+    result = subprocess.run([exec_path], capture_output=True, text=True)
+    print(f"Execution finished. Return code: {result.returncode}")
+    if result.stdout:
+        print("\n---- Captured STDOUT ----")
+        print(result.stdout.strip())
+        print("-------------------------\n")
+    
+    if result.stderr:
+        print("\n---- Captured STDERR ----")
+        print(result.stderr.strip())
+        print("-------------------------\n")
+        
+    return result
 
 def compile_benchmark(source_file, output_file, flags=[]):
 	"""Depending on the input compiles the uncompiled benchmark source file into an executable."""
@@ -21,5 +38,11 @@ def save_results(results):
 	"""Saves the benchmark results to a file. Maybe support automated analysis later in a separate function."""
 
 if __name__ == "__main__":
-   """Call run_benchmark with supplied flags/configs and handle results saving."""
-   pass
+    # Later on add config file support, for now keep it simple
+    parser = argparse.ArgumentParser(description="Run the amd-secure-bench tool for benchmarking secure AMD hardware.")
+    parser.add_argument("exec", help="Path to executable file.")
+
+    args = parser.parse_args()
+
+    results = run_benchmark(args.exec)
+    save_results(results)
