7cc729eb48ea32154919cb15475faf0a4cdda1d4
engelsm <marlon.engels21@gmail.com>
2025-12-14 17:46:59 +0100

    using uprof for profiling

--- eigen/eigenjob.sh
+++ eigen/eigenjob.sh
@@ -2,108 +2,42 @@
 #SBATCH --cpus-per-task=16
 
 module load lang/SciPy-bundle/2024.05-gfbf-2024a
-
-# ----------------------------------------
-# CONFIG
-# ----------------------------------------
+# module load AMDuProfCLI/VERSION
 CORES=(1 2 4 8 16)
-SAMPLE_RATE=5
+SAMPLE_RATE=1
+ALGORITHMS=("lanczos" "lobpcg")
 
-# ----------------------------------------
-# OUTPUT
-# ----------------------------------------
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-OUTDIR="outputs/benchmark_$TIMESTAMP"
+OUTDIR="outputs/uprof_benchmark_$TIMESTAMP"
 mkdir -p "$OUTDIR"
-CSV="$OUTDIR/benchmark_results.csv"
 
-echo "cores,run,algorithm,task_duration_ms,cycles,instructions,cache_refs,cache_misses,cpu_migrations,context_switches" > "$CSV"
-
-# ----------------------------------------
-# INPUT
-# ----------------------------------------
 MATRIX="matrices/bcsstk13.mtx"
 FORMATTED="matrices/formatted"
 BASE=$(basename "$MATRIX" .mtx)
-DENSE="$FORMATTED/${BASE}_dense.npy"
+#DENSE="$FORMATTED/${BASE}_dense.npy"
 SPARSE="$FORMATTED/${BASE}_sparse.npz"
 
 python3 src/load_matrix.py "$MATRIX"
 
-# ----------------------------------------
-# BENCHMARK
-# ----------------------------------------
 for C in "${CORES[@]}"; do
     export OMP_NUM_THREADS=$C
-    echo "--- Cores: $C ---"
 
+    echo "--- Cores: $C ---"
     for R in $(seq 1 $SAMPLE_RATE); do
         echo "  Run $R"
 
-        LANCZOS_OUT="$OUTDIR/${BASE}_lanczos.npy"
-        LOBPCG_OUT="$OUTDIR/${BASE}_lobpcg.npy"
+        for ALGO in "${ALGORITHMS[@]}"; do
+            UPROF_RUN_DIR="$OUTDIR/$ALGO\_C${C}_R${R}"
+            mkdir -p "$UPROF_RUN_DIR"
+            
+            ALGO_OUT_FILE="$UPROF_RUN_DIR/algo_results.npy"
 
-        # -------------------------
-        # LANCZOS
-        # -------------------------
-        PERF_LANCZOS=$(perf stat -x, \
-            -e task-clock,cycles,instructions,cache-references,cache-misses,cpu-migrations,context-switches \
-            python3 src/lanczos.py "$SPARSE" "$LANCZOS_OUT" \
-            2>&1 >/dev/null
-        )
+            echo "    > Profiling $ALGO (Saving to $UPROF_RUN_DIR)"
+            
+            AMDuProfCLI profile -o "$UPROF_RUN_DIR" --config overview python3 "src/$ALGO.py" "$SPARSE" "$ALGO_OUT_FILE"
 
-        echo "$PERF_LANCZOS" | awk -F, -v c="$C" -v r="$R" -v a="LANCZOS" '
-            /task-clock/       {td=$1}
-            /cycles/           {cy=$1}
-            /instructions/     {ins=$1}
-            /cache-references/ {cr=$1}
-            /cache-misses/     {cm=$1}
-            /cpu-migrations/   {mig=$1}
-            /context-switches/ {cs=$1}
-            END {print c","r","a","td","cy","ins","cr","cm","mig","cs}
-        ' >> "$CSV"
-
-        # -------------------------
-        # RQI
-        # -------------------------
-        PERF_RQI=$(perf stat -x, \
-            -e task-clock,cycles,instructions,cache-references,cache-misses,cpu-migrations,context-switches \
-            python3 src/rqi.py "$DENSE" "$LANCZOS_OUT" \
-            2>&1 >/dev/null
-        )
-
-        echo "$PERF_RQI" | awk -F, -v c="$C" -v r="$R" -v a="RQI" '
-            /task-clock/       {td=$1}
-            /cycles/           {cy=$1}
-            /instructions/     {ins=$1}
-            /cache-references/ {cr=$1}
-            /cache-misses/     {cm=$1}
-            /cpu-migrations/   {mig=$1}
-            /context-switches/ {cs=$1}
-            END {print c","r","a","td","cy","ins","cr","cm","mig","cs}
-        ' >> "$CSV"
-
-        # -------------------------
-        # LOBPCG
-        # -------------------------
-        PERF_LOBPCG=$(perf stat -x, \
-            -e task-clock,cycles,instructions,cache-references,cache-misses,cpu-migrations,context-switches \
-            python3 src/lobpcg.py "$SPARSE" "$LOBPCG_OUT" \
-            2>&1 >/dev/null
-        )
-
-        echo "$PERF_LOBPCG" | awk -F, -v c="$C" -v r="$R" -v a="LOBPCG" '
-            /task-clock/       {td=$1}
-            /cycles/           {cy=$1}
-            /instructions/     {ins=$1}
-            /cache-references/ {cr=$1}
-            /cache-misses/     {cm=$1}
-            /cpu-migrations/   {mig=$1}
-            /context-switches/ {cs=$1}
-            END {print c","r","a","td","cy","ins","cr","cm","mig","cs}
-        ' >> "$CSV"
-
+        done 
     done
 done
 
-echo "DONE → $CSV"
+echo "DONE"
--- eigen/src/a.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import scipy
-import scipy.linalg
-
-print("NumPy:", np.__config__.show())
-print("SciPy:", scipy.__config__.show())
