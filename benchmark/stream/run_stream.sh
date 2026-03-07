9156c08e7ef6204e2cb2a9f2fbe037aafebe5e83
engelsm <marlon.engels21@gmail.com>
2026-03-07 22:51:30 +0100

    stream stuff

--- benchmark/stream/run_stream.sh
+++ benchmark/stream/run_stream.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
 
+# Konfiguration
 STREAM_ARRAY_SIZE=430080000
 NTIMES=100
-ml mpi/OpenMPI/4.1.5-GCC-12.3.0
-gcc -o stream.out -O -DSTREAM_ARRAY_SIZE=$STREAM_ARRAY_SIZE -DNTIMES=$NTIMES -fopenmp -mcmodel=medium ./stream.c
-
 CSV_FILE="stream_results.csv"
-echo "Threads,Boost,VM,Host,Function,BestRate_MBs,AvgTime,MinTime,MaxTime" > "$CSV_FILE"
-
-declare -A VMS
-VMS=( ["ramses2007"]="ramses11002" ["ramses2004"]="ramses11041" )
-
 THREADS_LIST=(24 48 96)
-BOOST_STATES=("on" "off")
 
-for VM in "${!VMS[@]}"; do
-    HOST="${VMS[$VM]}"
+ENV=$1
+BOOST=$2
+NUMA=$3
+
+if [ ! -f "$CSV_FILE" ]; then
+    echo "Threads,Env,Boost,Numa,Function,BestRate_MBs,AvgTime,MinTime,MaxTime" > "$CSV_FILE"
+fi
+
+for N in "${THREADS_LIST[@]}"; do
+    echo "== Running STREAM with $N threads ($ENV, Numa: $NUMA) =="
     
-    for BOOST in "${BOOST_STATES[@]}"; do
-        echo "== Running $VM on $HOST with CPU boost $BOOST =="
+    export OMP_NUM_THREADS=$N
+    export OMP_PROC_BIND=close
+    export OMP_PLACES=cores
+    
+    if [ "$ENV" == "native" ]; then
+        if [ "$NUMA" == "interleave" ]; then
+            # Bestimme Nodes basierend auf Thread-Anzahl
+            case $N in
+                24) NODES="0" ;;
+                48) NODES="0,1" ;;
+                96) NODES="0,1,2,3" ;;
+                *)  NODES="all" ;; # Fallback
+            esac
+            CMD="numactl --interleave=$NODES -C 0-$((N-1)) ./stream.out"
+        else
+            # Standard: Nur CPU-Binding, Memory bleibt lokal (default)
+            CMD="numactl -C 0-$((N-1)) ./stream.out"
+        fi
+    else
+        # In der VM (SEV/SME) lassen wir das OS/Hypervisor entscheiden
+        CMD="./stream.out"
+    fi
 
-        ssh "$HOST" "sudo /usr/local/bin/cpu-boost-toggle.sh $BOOST"
-
-        for N in "${THREADS_LIST[@]}"; do
-            ssh "$VM" "
-                export OMP_NUM_THREADS=$N
-                export OMP_PROC_BIND=close
-                export OMP_PLACES=cores
-                ~/MA-bench-framework/benchmark/stream/stream.out
-            " | awk -v threads="$N" -v boost="$BOOST" -v vm="$VM" -v host="$HOST" '
-                /Copy:|Scale:|Add:|Triad:/ {
-                    sub(/:/, "", $1);
-                    print threads "," boost "," vm "," host "," $1 "," $2 "," $3 "," $4 "," $5
-                }' >> "$CSV_FILE"
-        done
-    done
+    echo "Executing: $CMD"
+    $CMD | awk -v threads="$N" -v env="$ENV" -v boost="$BOOST" -v numa="$NUMA" '
+        /Copy:|Scale:|Add:|Triad:/ {
+            sub(/:/, "", $1);
+            print threads "," env "," boost "," numa "," $1 "," $2 "," $3 "," $4 "," $5
+        }' >> "$CSV_FILE"
 done
 
-echo "All results saved to $CSV_FILE"
+echo "Done. Results appended to $CSV_FILE"
