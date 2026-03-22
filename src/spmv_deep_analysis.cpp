073dc56539e52cfa10d19f7d6e1552a4aa1e1899
engelsm <marlon.engels21@gmail.com>
2026-03-22 10:42:55 +0100

    restructuring the spmv bench workflow

--- src/spmv_deep_analysis.cpp
+++ src/spmv_deep_analysis.cpp
@@ -8,13 +8,33 @@
 #include <numeric>
 #include <filesystem>
 #include "util.hpp"
-#include "perf_util.hpp"
+#include "util_perf.hpp"
+
+std::string results_file_name = "results.csv";
+std::string iter_file_name = "iter.csv";
+
+/**
+ * Perform Sparse Matrix-Vector Multiplication (SpMV) using CSR format
+ * https://en.wikipedia.org/wiki/Sparse_matrix#Compressed_sparse_row_(CSR,_CRS_or_Yale_format)
+ */
+void spmv_csr(int rows, const int *row_ptr, const int *col_idx, const Scalar *values, const CustomVector &x, CustomVector &y)
+{
+// Parallelize over rows
+#pragma omp parallel for schedule(static)
+	for (int i = 0; i < rows; i++)
+	{
+		Scalar sum = 0;
+		for (int j = row_ptr[i]; j < row_ptr[i + 1]; j++)
+			sum += values[j] * x[col_idx[j]];
+		y[i] = sum;
+	}
+}
 
 int main(int argc, char **argv)
 {
-	if (argc < 8)
+	if (argc < 7)
 	{
-		std::cerr << "Usage: " << argv[0] << " <matrix.bin> <iterations> <NUMA_opt 0/1> <run_id> <cores> <stats_csv> <output_dir>\n";
+		std::cerr << "Usage: " << argv[0] << " <matrix.bin> <iterations> <NUMA_opt 0/1> <run_id> <cores> <output_dir>\n";
 		return 1;
 	}
 
@@ -23,14 +43,14 @@ int main(int argc, char **argv)
 	bool NUMA_optimize = (std::stoi(argv[3]) != 0);
 	int run_id = std::stoi(argv[4]);
 	int num_cores = std::stoi(argv[5]);
-	std::string stats_csv_path = argv[6];
-	std::string output_dir = argv[7];
+	std::string output_dir = argv[6];
 
 	std::string matrix_basename = std::filesystem::path(matrix_full_path).filename().string();
 
 	srand(42);
 	omp_set_num_threads(num_cores);
 
+	// IO is measured with std::chrono to get the actual whole time spent, including all overheads in non-user mode.
 	auto io_start = std::chrono::high_resolution_clock::now();
 	CustomSparseMatrix A = load_binary_matrix(matrix_full_path, NUMA_optimize);
 	auto io_end = std::chrono::high_resolution_clock::now();
@@ -67,23 +87,18 @@ int main(int argc, char **argv)
 	iter_results.reserve(max_iterations);
 
 	PerfGroup pg;
+	// Events are defined in perf_event.h by the Linux kernel
 	pg.add_event(PERF_TYPE_HARDWARE, PERF_COUNT_HW_CPU_CYCLES, "cycles");
 	pg.add_event(PERF_TYPE_HARDWARE, PERF_COUNT_HW_INSTRUCTIONS, "instructions");
 	pg.add_event(PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES, "cache_misses");
-	pg.add_event(PERF_TYPE_HW_CACHE, (PERF_COUNT_HW_CACHE_DTLB | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16)), "dtlb_misses");
+	// https://stackoverflow.com/questions/61190033/how-to-measure-the-dtlb-hits-and-dtlb-misses-with-perf-event-open
+	pg.add_event(PERF_TYPE_HW_CACHE, (PERF_COUNT_HW_CACHE_DTLB | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16)), "dtlb_load_misses");
 
 	pg.start();
 	for (int iter = 0; iter < max_iterations; ++iter)
 	{
 		double t_start = omp_get_wtime();
-#pragma omp parallel for schedule(static)
-		for (int i = 0; i < A.rows(); i++)
-		{
-			Scalar sum = 0;
-			for (int j = row_ptr[i]; j < row_ptr[i + 1]; j++)
-				sum += values[j] * x[col_idx[j]];
-			y[i] = sum;
-		}
+		spmv_csr(A.rows(), row_ptr, col_idx, values, x, y);
 		double t_end = omp_get_wtime();
 		double elapsed = t_end - t_start;
 		iter_results.push_back({elapsed, (2.0 * A.nonZeros()) / (elapsed * 1e9)});
@@ -101,8 +116,9 @@ int main(int argc, char **argv)
 		hw_vals.push_back(pg.get_value(e.fd));
 	}
 
-	// Write global statistics (Append mode)
-	std::ofstream stats_file(stats_csv_path, std::ios::app);
+	// Write aggregated statistics
+	std::string results_file_path = output_dir + "/" + results_file_name;
+	std::ofstream stats_file(results_file_path, std::ios::app);
 	if (stats_file.is_open())
 	{
 		stats_file << matrix_basename << ","
@@ -119,15 +135,18 @@ int main(int argc, char **argv)
 		stats_file.close();
 	}
 
-	// Write detailed iteration results (New file per run)
-	std::string iter_file_path = output_dir + "/run_" + std::to_string(run_id) + "_c" + std::to_string(num_cores) + ".csv";
-	std::ofstream iter_file(iter_file_path);
+	// Write detailed iteration results
+	std::string iter_file_path = output_dir + "/" + iter_file_name;
+	std::ofstream iter_file(iter_file_path, std::ios::app);
+
 	if (iter_file.is_open())
 	{
-		iter_file << "Iter,Time,GFLOPS\n";
 		for (size_t i = 0; i < iter_results.size(); ++i)
 		{
-			iter_file << i + 1 << "," << iter_results[i].elapsed << "," << iter_results[i].gflops << "\n";
+			iter_file << run_id << ","
+					  << i + 1 << ","
+					  << iter_results[i].elapsed << ","
+					  << iter_results[i].gflops << "\n";
 		}
 		iter_file.close();
 	}
---
+++ src/util_perf.hpp
@@ -0,0 +1,78 @@
+#pragma once
+#include <linux/perf_event.h>
+#include <sys/syscall.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <vector>
+#include <cstring>
+#include <string>
+
+// Documentation: https://man7.org/linux/man-pages/man2/perf_event_open.2.html
+
+class PerfGroup
+{
+public:
+	struct Event
+	{
+		int fd; // https://en.wikipedia.org/wiki/File_descriptor
+		std::string name;
+	};
+	// Perf events need to be grouped together to ensure they are measured simultaneously.
+	// The first added event becomes the group leader, and all subsequent events are added to this group.
+	// This grouping is handled by the OS in the syscall in the add_event() method.
+	std::vector<Event> events;
+	int leader_fd = -1;
+
+	void add_event(uint32_t type, uint64_t config, std::string name)
+	{
+		perf_event_attr attr;
+		std::memset(&attr, 0, sizeof(attr)); // Zero out the structure to ensure all fields are initialized as otherwise they might contain garbage values
+		attr.type = type;					 // Type of the event (e.g., hardware, software, cache, etc.)
+		attr.size = sizeof(attr);			 // Size of the perf_event_attr structure
+		attr.config = config;				 // Specific event to monitor (e.g., CPU cycles, instructions, cache misses, etc.)
+		attr.disabled = 1;					 // Start in a disabled state; we will enable it later with ioctl, thus we can isolate measurements
+		attr.inherit = 1;					 // Inherit event counters to child processes (openMP threads)
+		attr.exclude_kernel = 1;			 // Exclude events that occur in kernel mode
+		attr.exclude_hv = 1;				 // Exclude events that occur in hypervisor mode
+
+		//"glibc provides no wrapper for perf_event_open(), necessitating the use of syscall(2)." (perf_event_open manual)
+		int fd = syscall(__NR_perf_event_open, &attr, 0, -1, leader_fd, 0); // pid=0 (self), cpu=-1 (all CPUs), group_fd=leader_fd (grouping events), flags=0
+		if (fd == -1)
+		{
+			perror("perf_event_open failed");
+			return;
+		}
+		if (leader_fd == -1) // The first added event becomes the group leader
+			leader_fd = fd;
+		events.push_back({fd, name});
+	}
+
+	// Needs to be called immediately before the measured code section. Resets and starts all event measurements in the group simultaneously.
+	void start()
+	{
+		ioctl(leader_fd, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP);
+		ioctl(leader_fd, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP);
+	}
+
+	// Needs to be called immediately after the measured code section to stop the measurements.
+	void stop()
+	{
+		ioctl(leader_fd, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP);
+	}
+
+	// Reads the value of a specific event file descriptor. Can be called after stop() to get the final count for that event.
+	long long get_value(int fd)
+	{
+		long long val;
+		if (read(fd, &val, sizeof(long long)) == -1)
+			return -1;
+		return val;
+	}
+
+	// Destructor to close all event file descriptors when the PerfGroup object goes out of scope.
+	~PerfGroup()
+	{
+		for (auto &e : events)
+			close(e.fd);
+	}
+};
